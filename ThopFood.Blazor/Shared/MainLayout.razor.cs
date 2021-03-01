4594dd4d15b9434c2a9b3b3900beb97c2f08494c
Thomas Hop <thomas.hhopp@gmail.com>
2021-03-01 19:52:02 +0100

    Added MudBlazor with mudblazor layout

---
+++ ThopFood.Blazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor;
+
+namespace ThopFood.Blazor.Shared
+{
+    public partial class MainLayout
+    {
+        public MudTheme MudTheme = new MudTheme
+        {
+            Palette = new Palette
+            {
+                Primary = "#8FC93A",
+                AppbarBackground = "#8FC93A",
+                Secondary = "#508CA4",
+                Background = "#F6FAFA",
+                BackgroundGrey = "#F0F7F4"
+            }
+        };
+
+        private bool _drawerOpen = true;
+
+        public void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
