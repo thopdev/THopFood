9dce143ea64ed225ff10fd92b116fb8c20cef0b7
Thomas Hop <thomas.hhopp@gmail.com>
2021-03-08 20:38:30 +0100

    Added Profile Card

---
+++ ThopFood.Blazor/Components/Profile/ProfileComponent.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+using ThopFood.Blazor.Models;
+
+namespace ThopFood.Blazor.Components.Profile
+{
+    public partial class ProfileComponent
+    {
+        public UserProfile Profile { get; set; } = new UserProfile
+        {
+            Color = Color.Primary,
+            Name = "Thomas",
+            UserName = "@ThopDev",
+            Followers = 425,
+            Rating = 4,
+            RatingCount = 10000,
+            Following= true
+        };
+
+
+
+        public void FollowButton()
+        {
+            Profile.Following = !Profile.Following;
+
+            if (Profile.Following)
+            {
+                Profile.Followers++;
+            }
+            else
+            {
+                Profile.Followers--;
+            }
+        }
+
+    }
+}
