ee9970ab8df6e3a54e9292650b93dd6b4315181f
Thomas Hop <thomas.hhopp@gmail.com>
2021-03-08 21:03:09 +0100

    Added API and Shared project!

---
+++ ThopFood.API/Controllers/RecipeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThopFood.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+    }
+}
