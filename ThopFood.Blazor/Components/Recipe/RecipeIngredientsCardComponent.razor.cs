f9af5461426d8aca06292f8db22cd8370322115f
Thomas Hop <thomas.hhopp@gmail.com>
2021-03-08 21:40:52 +0100

    Added ingredient and utensils

---
+++ ThopFood.Blazor/Components/Recipe/RecipeIngredientsCardComponent.razor.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace ThopFood.Blazor.Components.Recipe
+{
+    public partial class RecipeIngredientsCardComponent
+    {
+        public List<RecipeIngredient> Ingredients { get; set; } = new List<RecipeIngredient>
+        {
+            new RecipeIngredient
+                {Ingredient = new Ingredient {Name = "Garlic", Type = IngredientType.Amount}, Amount = 5},
+            new RecipeIngredient
+                {Ingredient = new Ingredient {Name = "Water", Type = IngredientType.Volume}, Amount = 1000},
+            new RecipeIngredient
+                {Ingredient = new Ingredient {Name = "Flour", Type = IngredientType.Weight}, Amount = 1000000},
+
+        };
+
+        public List<RecipeUtensil> Utensils { get; set; } = new List<RecipeUtensil>(new List<RecipeUtensil>
+        {
+            new RecipeUtensil {Name = "AirFryer"},
+            new RecipeUtensil {Name = "Spoon"},
+            new RecipeUtensil{Name = "Saute Pan"},
+            new RecipeUtensil{Name = "BBQ"},
+            new RecipeUtensil{Name = "Dutch Oven"}
+        });
+    }
+}
---
+++ ThopFood.Blazor/Models/Ingredient.cs
@@ -0,0 +1,8 @@
+namespace ThopFood.Blazor.Components.Recipe
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public IngredientType Type { get; set; }
+    }
+}
