9d654598010c71955aaef8b17d79933c1efaf32d
Thomas Hop <thomas.hhopp@gmail.com>
2021-03-15 21:18:30 +0100

    Add Sql Server with entity framework

---
+++ ThopFood.API/Data/ApplicationDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThopFood.API.Data.Entities;
+
+namespace ThopFood.API.Data
+{
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ ThopFood.API/Data/Entities/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ThopFood.Shared.Dtos.Recipes;
+namespace ThopFood.API.Data.Entities
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool Favorite { get; set; }
+
+        public int OwnerId { get; set; }
+    }
+}
