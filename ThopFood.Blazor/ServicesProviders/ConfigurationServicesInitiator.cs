e633854463371e8d59ccba992d5ebb97f760accc
Thomas Hop <thomas.hhopp@gmail.com>
2021-04-19 20:18:07 +0200

    added service provider for blazor

---
+++ ThopFood.Blazor/ServicesProviders/ConfigurationServicesInitiator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ThopFood.Blazor.ServicesProviders
+{
+    public class ConfigurationServicesInitiator : IServiceInitiator
+    {
+        public void InitiateService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44377/api") });
+
+            serviceCollection.AddSingleton(new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
---
+++ ThopFood.Blazor/ServicesProviders/ServicesInitiator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ThopFood.Blazor.Services;
+
+namespace ThopFood.Blazor.ServicesProviders
+{
+    public class ServicesInitiator : IServiceInitiator
+    {
+        public void InitiateService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IJsonService, JsonService>();
+        }
+    }
+}
