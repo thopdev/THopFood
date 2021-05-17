cec573c0ee33983f5692e1a865122b001f4b10bf
Thomas Hop <thomas.hhopp@gmail.com>
2021-05-17 19:03:44 +0200

    added more tests

---
+++ ThopFood.Blazor.BUnit/Utils/BunitVerifyInitiator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Verify.AngleSharp;
+using VerifyTests;
+
+namespace ThopFood.Blazor.BUnit.Utils
+{
+    public static class BunitVerifyInitiator
+    {
+       
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            // remove some noise from the html snapshot
+            VerifierSettings.ScrubEmptyLines();
+            VerifierSettings.ScrubLinesWithReplace(s => s.Replace("<!--!-->", ""));
+            HtmlPrettyPrint.All();
+
+            VerifierSettings.ScrubLinesContaining("<script src=\"_framework/dotnet.");
+        }
+        }
+
+
+    }
+
